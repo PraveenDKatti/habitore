185801fe7cdbce912ae6d8182e9d06c9300bc357
Praveen Katti <praveendk25@gmail.com>
2025-12-05 13:34:42 +0530

    wrap the entire application in the BrowserRouter to enable navigation features, and updated app with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// Layout Components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from './pages/Home'
 
+// Pages
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-background">
+      {/* Navbar stays constant */}
       <Navbar />
       
       <main className="flex-1">
-        <Home />
+        <Routes>
+          {/* URL: / */}
+          <Route path="/" element={<Home />} />
+        </Routes>
       </main>
 
+      {/* Footer stays constant */}
       <Footer />
     </div>
   );
